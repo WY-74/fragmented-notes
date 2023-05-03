024d44aaf1eb9d38b76a5da6e88a066763d5d8d6
wangyun-aw <wangyun@awesomego.com.cn>
2023-05-03 18:32:25 +0800

    230503: update pytest

--- case/pytest_allure/conftest.py
+++ case/pytest_allure/conftest.py
@@ -1,7 +1,15 @@
 import pytest
+from typing import List
 
 from case.pytest_allure.func.calculator import Calculator
+
 
 @pytest.fixture(scope="session")
 def calculator():
     return Calculator()
+
+
+def pytest_collection_modifyitems(session: "Session", config: "Config", items: List["Item"]) -> None:
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
