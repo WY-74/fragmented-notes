44d983bd8a78b303585b6a899b5b5eb053cc34b5
WangYun <294961525@qq.com>
2023-05-19 09:56:45 +0800

    230519: update python selenium

--- case/python_selenium/utils/instantiate_driver.py
+++ case/python_selenium/utils/instantiate_driver.py
@@ -1,14 +1,27 @@
 from selenium import webdriver
-from utils.env import ENV
+from conftest import ENV
+from utils.capabilities import CAPABILITIES
+
 
 def instantiate_driver():
-    options = webdriver.ChromeOptions()
-    options.page_load_strategy = 'eager'
-    if ENV.CHROME_DEBUG:
-        options.debugger_address = "localhost:9000"
+    browser = ENV["browser"]
 
-    driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
-    driver.implicitly_wait(3)
+    if not ENV["remote"]:
+        options = getattr(webdriver, f"{browser}Options")()
+        options.page_load_strategy = 'eager'
+        if ENV["headless"]:
+            options.add_argument('--headless')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--disable-dev-shm-usage')
+        if ENV["debugger"]:
+            options.debugger_address = "localhost:9000"
+
+        driver = getattr(webdriver, ENV["browser"])(options=options)
+        driver.maximize_window()
+        driver.implicitly_wait(3)
+
+    hogwarts_grid_url = "https://selenium-node.hogwarts.ceshiren.com/wd/hub"  # 这是一个暂时的节点终端
+    capabilities = CAPABILITIES
+    driver = webdriver.Remote(command_executor=hogwarts_grid_url, desired_capabilities=capabilities)
 
     return driver
